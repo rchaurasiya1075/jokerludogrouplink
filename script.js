69531c5f0fb76adfdaa4d3a0a4235efd4dc68ade
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2026-02-22 16:30:43 +0530

    Add mobile menu toggle functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("mobileMenu");
+
+toggle.addEventListener("click", () => {
+menu.classList.toggle("active");
+});
